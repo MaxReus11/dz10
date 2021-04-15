60daa4a1c6df33a8deadb3409ded821c76926246
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-04-16 01:31:58 +0300

    update server class

--- 1_server.cpp
+++ 1_server.cpp
@@ -13,42 +13,33 @@
 #include <boost/interprocess/containers/map.hpp>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/asio.hpp>
-std::string read_data(boost::asio::ip::tcp::socket& socket)
-{
-    const std::size_t length = 10;
-    char buffer[length];
-    boost::asio::read(socket, boost::asio::buffer(buffer, length));
-    return std::string(buffer, length);
-}
-
 
 using namespace boost::interprocess;
 using char_allocator = allocator<char, managed_shared_memory::segment_manager>;
 using shared_string = boost::interprocess::basic_string<char, std::char_traits<char>, char_allocator>;
-using pair_str_allocator = allocator < std::pair< std::string, shared_string>, managed_shared_memory::segment_manager >;
-using pair_allocator = allocator < std::pair<int, std::pair< std::string, shared_string>>, managed_shared_memory::segment_manager >;
-using shared_map = boost::unordered_map< int, std::pair< std::string, shared_string>, boost::hash<int>, std::equal_to<int>, pair_allocator>;
+using pair_allocator = allocator < std::pair<int, std::pair< std::string, std::string>>, managed_shared_memory::segment_manager >;
+using shared_map = boost::unordered_map< int, std::pair< std::string, std::string>, boost::hash<int>, std::equal_to<int>, pair_allocator>;
 
 class Server {
 private:
     std::vector< std::future<void>> thr;
     interprocess_mutex* mutex_;
     interprocess_condition* condition_;
-    std::vector<std::pair<std::string, std::string>> data;
     std::atomic<bool> flag = false, end = false;
     shared_map* map_;
     int* ID_;
     shared_string* line;
-    std::string personal_name;
-    shared_string* current_name;
+   /* std::string personal_name;
+    shared_string* current_name;*/
+    const std::string shared_memory_name = "managed_shared_memory";
+    managed_shared_memory shared_memory;
 public:
-    void add(boost::asio::ip::tcp::socket& socket)
+    void add(boost::asio::ip::tcp::socket* socket)
     {
         std::string message;
         do {
             boost::asio::streambuf str_buf;
             boost::asio::streambuf name_buf;
-            
             std::string name_str;
             size_t name_size;
             name_size = boost::asio::read_until(socket, name_buf, ' ');
@@ -58,48 +49,79 @@ public:
             std::istream input_stream(&name_buf);
             std::getline(input_stream, name_str);
             std::scoped_lock lock(mutex_);
-            data.push_back(std::make_pair(name_str, message));
+           map_->emplace(*ID_, std::make_pair(name_str, message));
+           (*ID_)++;
             flag = true;
             condition_->notify_all();
         } while (message != "Bye");
         end = true;
     }
-    void out() {
+    void out(boost::asio::ip::tcp::endpoint endpoint) {
         while (!end.load()) {
             std::unique_lock lock(*mutex_);
             condition_->wait(lock);
             if (!flag.load()) {
-                std::cout << "[" <<map_->find((*ID_) - 1)->second.first << "]: " << (map_->find((*ID_) - 1))->second.second << std::endl;
+                try
+                {
+                    boost::asio::io_service io_service;
+                    boost::asio::ip::tcp::socket socket(io_service, endpoint.protocol());
+                    socket.connect(endpoint);
+                    boost::asio::write(socket,
+                        boost::asio::buffer("[" + map_->find(((*ID_) - 1))->second.first + "]: " + (map_->find((*ID_) - 1))->second.second));
+
+                }
+                catch (boost::system::system_error& e)
+                {
+                    std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
+                    system("pause");
+                    e.code().value();
+                }  
             }
             flag = false;
         }
     }
-    void processing(boost::asio::ip::tcp::socket& socket) {
-        const std::string shared_memory_name = "managed_shared_memory";
-        managed_shared_memory shared_memory;
-        interprocess_mutex* mutex_;
-        interprocess_condition* condition_;
+    void processing(boost::asio::ip::tcp::socket* socket, boost::asio::ip::tcp::endpoint endpoint){
+       /* for (auto i : *map_) {
+            std::cout << "[" << i.second.first << "]: " << i.second.second << std::endl;
+        }*/
+        std::future<void> th1 = std::async(&Server::add, this, socket);
+        std::future<void> th2 = std::async(&Server::out, this, endpoint);
+        using namespace std::chrono_literals;
+        while (!end.load()) {
+            std::this_thread::sleep_for(2ms);
+        }
+            th1.get();
+            th2.get();
+        system("pause");
     }
     void start() {
+        shared_memory = managed_shared_memory(open_or_create, shared_memory_name.c_str(), 4096);
+        mutex_ = shared_memory.find_or_construct<interprocess_mutex>("m")();
+        condition_ = shared_memory.find_or_construct<interprocess_condition>("c")();
+        map_ = shared_memory.find_or_construct<shared_map>("Users")(shared_memory.get_segment_manager());
+        ID_ = shared_memory.find_or_construct<int>("ID")(0);
+        line = shared_memory.find_or_construct<shared_string>("line")(shared_memory.get_segment_manager());
         const std::size_t size = 30;
         auto port = 3333;
         boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
         boost::asio::io_service io_service;
-        try
-        {
-            boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
-            acceptor.bind(endpoint);
-            acceptor.listen(size);
-            boost::asio::ip::tcp::socket socket(io_service);
-            acceptor.accept(socket);
-            thr.emplace_back(std::async(&Server::processing, socket));
-        }
-        catch (boost::system::system_error& e)
-        {
-            std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
-            system("pause");
-            return e.code().value();
-        }
+        do {
+            try
+            {
+                boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
+                acceptor.bind(endpoint);
+                acceptor.listen(size);
+                boost::asio::ip::tcp::socket socket(io_service);
+                acceptor.accept(socket);
+                thr.emplace_back(std::async(&Server::processing,this, &socket, endpoint));
+            }
+            catch (boost::system::system_error& e)
+            {
+                std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
+                system("pause");
+                e.code().value();
+            }
+        }while(*line!= "Close server");
     }
 };
 int main() {
