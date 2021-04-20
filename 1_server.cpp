edb389a80c59644cf770541b54b4601ff9125531
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-04-21 01:49:27 +0300

    try to fix memory_access problems

--- 1_server.cpp
+++ 1_server.cpp
@@ -1,30 +1,77 @@
+#define BOOST_DATE_TIME_NO_LIB
 #include <iostream>
 #include <vector>
 #include <boost/asio.hpp>
 #include <future>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 class ClientOnServer {
 private:
 	
 	boost::asio::ip::tcp::socket m_socket;
 	std::string personal_name;
-	
-public:
+	std::mutex m_mutex;
 	std::string m_message;
-	ClientOnServer(boost::asio::ip::tcp::socket socket, std::atomic<bool>* flag) : m_socket(std::move(socket)) {
+	std::atomic<bool> m_flag;
+public:
+	
+	ClientOnServer(boost::asio::ip::tcp::socket socket, std::condition_variable* con_, std::string* message_, std::mutex* mutex_, 
+		std::atomic<bool>* flag) : m_socket(std::move(socket)) {
 		boost::asio::streambuf str_buf;
 		boost::asio::read_until(m_socket, str_buf, '\n');
 		std::istream input_stream(&str_buf);
 		std::getline(input_stream, personal_name);
+		std::future<void> thr_out = std::async(&ClientOnServer::send, this, con_, message_);
+		std::future<void> thr_get = std::async(&ClientOnServer::get, this, con_, message_, mutex_);
+		thr_out.get();
+		thr_get.get();
 		*flag = true;
 	}
-	std::string get_name() {
+	/*std::string get_name() {
 		return this->personal_name;
 	}
 	void add_message(std::string* message_) {
 		m_message = *message_;
+	}*/
+	void get(std::condition_variable* con_, std::string* message_, std::mutex* mutex_) {
+		while (true) {
+			boost::asio::streambuf str_buf;
+			m_message = *message_;
+			boost::asio::read_until(m_socket, str_buf, '\n');
+			std::istream input_stream(&str_buf);
+			std::getline(input_stream, m_message);
+			if (m_message == "EOF") {
+				std::lock_guard<std::mutex> lock(*mutex_);
+				*message_ = personal_name + "\r";
+				con_->notify_all();
+				break;
+			}
+			m_flag = true;
+			std::scoped_lock lock(*mutex_);
+			*message_ = m_message;
+			con_->notify_all();
+		}
 	}
-	
+	void send(std::condition_variable* con_, std::string* message_) {
+		while (true) {
+			std::unique_lock lock(m_mutex);
+			con_->wait(lock);
+			m_message = *message_;
+			if (m_message == personal_name + '\r') {
+				boost::asio::write(m_socket, boost::asio::buffer("EOF\n"));
+				break;
+			}
+			if (m_message[m_message.size() - 1] == '\r') {
+				m_message.erase(m_message.size() - 1);
+				boost::asio::write(m_socket, boost::asio::buffer(m_message + " leave the chat\n"));
+			}
+			else {
+				if (!m_flag) boost::asio::write(m_socket, boost::asio::buffer(m_message + "\n"));
+				else m_flag = false;
+			}
+		}
+	}
 	
 };
 class Server {
@@ -53,48 +100,8 @@ public:
 			return;
 		}
 		std::future<void> tr_start = std::async(&Server::start, this);
-		ClientOnServer client(std::move(socket), &flag);
-		std::future<void> tr_get = std::async(&Server::get, this, std::move(socket), &client);
-		std::future<void> tr_send = std::async(&Server::send, this, std::move(socket), &client);
+		ClientOnServer client(std::move(socket), con_, &message_, &mutex_, &flag);
 		tr_start.get();
-	}
-	void get(boost::asio::ip::tcp::socket socket,ClientOnServer* client) {
-		while (true) {
-			boost::asio::streambuf str_buf;
-			client->add_message(&message_);
-			boost::asio::read_until(socket, str_buf, '\n');
-			std::istream input_stream(&str_buf);
-			std::getline(input_stream, client->m_message);
-			if (client->m_message == "EOF") {
-				std::lock_guard<std::mutex> lock(mutex_);
-				message_ = client->get_name() + "\r";
-				con_->notify_all();
-				break;
-			}
-			flag = true;
-			std::scoped_lock lock(mutex_);
-			message_ = client->m_message;
-			con_->notify_all();
-		}
-	}
-	void send(boost::asio::ip::tcp::socket socket, ClientOnServer* client) {
-		while (true) {
-			std::unique_lock lock(mutex_);
-			con_->wait(lock);
-			 client->add_message(&message_);
-			if (client->m_message == client->get_name() + '\r') {
-				boost::asio::write(socket, boost::asio::buffer("EOF\n"));
-				break;
-			}
-			if (client->m_message[client->m_message.size() - 1] == '\r'){
-				client->m_message.erase(client->m_message.size() - 1);
-				boost::asio::write(socket, boost::asio::buffer(client->m_message + " leave the chat\n"));
-			}
-			else {
-				if (!flag) boost::asio::write(socket, boost::asio::buffer(client->m_message + "\n"));
-				else flag = false;
-			}
-		}
 	}
 };
 
