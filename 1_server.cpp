bdaa8bd661b18786307f65b2c2be71c4b2a55608
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-04-21 01:24:47 +0300

    without saving messages and proccessing()

--- 1_server.cpp
+++ 1_server.cpp
@@ -1,140 +1,106 @@
-#define BOOST_DATE_TIME_NO_LIB
-#include<iostream>
-#include<string>
-#include <future>
-#include<chrono>
-#include<map>
-#include<atomic>
-#include <thread>
+#include <iostream>
+#include <vector>
 #include <boost/asio.hpp>
+#include <future>
+#include <thread>
 class ClientOnServer {
 private:
-    std::string personal_name;
-    boost::asio::ip::tcp::socket m_socket;
-    std::atomic<bool> flag;
+	
+	boost::asio::ip::tcp::socket m_socket;
+	std::string personal_name;
+	
 public:
-    ClientOnServer(boost::asio::ip::tcp::socket socket,
-         std::atomic<bool>* flag) : m_socket(std::move(socket)) {
-        boost::asio::streambuf buffer;
-        boost::asio::read_until(m_socket, buffer, '\n');
-        std::istream input_stream(&buffer);
-        std::getline(input_stream, personal_name);
-        *flag = true;
-    }
-    std::string get_name() {
-        return this->personal_name;
-    }
+	std::string m_message;
+	ClientOnServer(boost::asio::ip::tcp::socket socket, std::atomic<bool>* flag) : m_socket(std::move(socket)) {
+		boost::asio::streambuf str_buf;
+		boost::asio::read_until(m_socket, str_buf, '\n');
+		std::istream input_stream(&str_buf);
+		std::getline(input_stream, personal_name);
+		*flag = true;
+	}
+	std::string get_name() {
+		return this->personal_name;
+	}
+	void add_message(std::string* message_) {
+		m_message = *message_;
+	}
+	
+	
 };
-
 class Server {
 private:
-    std::mutex* mutex_;
-    std::condition_variable* con_;
-    boost::asio::ip::tcp::endpoint endpoint;
-    boost::asio::ip::tcp::acceptor acceptor;
-    boost::asio::io_service io_service;
-    std::vector<std::future<void>> thr;
-    std::atomic<bool> flag = false;
-    std::atomic<bool> close_flag = false;
-    int ID = 0;
-    std::map<int, std::pair<std::string, std::string>> map_;
-    const std::size_t size = 30;
-    int port = 3333;
-    std::string* message_;
-    const std::string close_str = "Close server";
+	int port = 3333;
+	std::string localhost = "127.0.0.1";
+	const std::size_t size = 30;
+	boost::asio::ip::tcp::endpoint endpoint;
+	boost::asio::io_service io_service;
+	std::condition_variable* con_;
+	boost::asio::ip::tcp::acceptor acceptor;
+	std::string message_= " ";
+	std::mutex mutex_;
+	std::atomic<bool> close_flag = false;
+	std::atomic<bool> flag = false;
 public:
-    Server():endpoint(boost::asio::ip::address_v4::any(), port), acceptor(io_service, endpoint.protocol()) {
-        acceptor.bind(endpoint);
-        acceptor.listen(size);
-    }
-    void start() {
-        do {
-            try
-            {
-                boost::asio::ip::tcp::socket socket(io_service);
-                 if (!close_flag.load()) {
-                    acceptor.accept(socket);
-                    thr.push_back(std::async(&Server::processing, this, std::move(socket), std::move(endpoint)));
-                    thr.back().get();
-                }
-                
-            }
-            catch (boost::system::system_error& server_start)
-            {
-                std::cout << "Error occured! Error code = " << server_start.code() << ". Message: " << server_start.what() << std::endl;
-                system("pause");
-                server_start.code().value();
-            }
-        } while (!close_flag.load());
-    }
-    
-    void processing(boost::asio::ip::tcp::socket socket, boost::asio::ip::tcp::endpoint endpoint){
-        try {
-            ClientOnServer cl(std::move(socket), &flag);
-            std::atomic<bool> end = false;
-            std::future<void> th1 = std::async(&Server::get, this, std::move(socket), &end, &cl);
-            std::future<void> th2 = std::async(&Server::send, this, std::move(socket), std::move(endpoint), &end);
-            th1.get();
-            th2.get();
-        }
-        catch (boost::system::system_error& server_processing)
-        {
-            std::cout << "Error occured! Error code = " << server_processing.code() << ". Message: " << server_processing.what() << std::endl;
-            system("pause");
-            server_processing.code().value();
-        }
-        system("pause");
-    }
-    void get(boost::asio::ip::tcp::socket socket, std::atomic<bool> end, ClientOnServer* cl)
-    {
-
-        do {
-            try {
-                std::scoped_lock lock(*mutex_);
-                boost::asio::streambuf str_buf;
-                boost::asio::read_until(socket, str_buf, '\n');
-                std::istream input_str(&str_buf);
-                std::getline(input_str, *message_);
-                map_.emplace(ID, std::make_pair(cl->get_name(), *message_));
-                ID++;
-                flag = true;
-                con_->notify_all();
-                if (*message_ == close_str) close_flag = true;
-            }
-            catch (boost::system::system_error& server_get)
-            {
-                std::cout << "Error occured! Error code = " << server_get.code() << ". Message: " << server_get.what() << std::endl;
-                system("pause");
-                server_get.code().value();
-            }
-        } while (*message_ != "Bye");
-        end = true;
-    }
-    void send(boost::asio::ip::tcp::socket socket, boost::asio::ip::tcp::endpoint endpoint, std::atomic<bool> end) {
-        while (!end.load()) {
-            std::unique_lock lock(*mutex_);
-            con_->wait(lock);
-            if (!close_flag.load()) {
-                try
-                {
-                    boost::asio::write(socket,
-                        boost::asio::buffer("[" + map_.find(ID - 1)->second.first + "]: " + (map_.find(ID - 1)->second.second)));
-
-                }
-                catch (boost::system::system_error& server_send)
-                {
-                    std::cout << "Error occured! Error code = " << server_send.code() << ". Message: " << server_send.what() << std::endl;
-                    system("pause");
-                    server_send.code().value();
-                }
-            }
-            flag = false;
-        }
-    }
+	Server() : endpoint(boost::asio::ip::address_v4::any(), port), acceptor(io_service, endpoint.protocol()) {
+		acceptor.bind(endpoint);
+		acceptor.listen(size);
+	}
+	void start() {
+		boost::asio::ip::tcp::socket socket(io_service);
+		acceptor.accept(socket);
+		std::atomic<bool> flag = false;
+		if (close_flag.load()) {
+			return;
+		}
+		std::future<void> tr_start = std::async(&Server::start, this);
+		ClientOnServer client(std::move(socket), &flag);
+		std::future<void> tr_get = std::async(&Server::get, this, std::move(socket), &client);
+		std::future<void> tr_send = std::async(&Server::send, this, std::move(socket), &client);
+		tr_start.get();
+	}
+	void get(boost::asio::ip::tcp::socket socket,ClientOnServer* client) {
+		while (true) {
+			boost::asio::streambuf str_buf;
+			client->add_message(&message_);
+			boost::asio::read_until(socket, str_buf, '\n');
+			std::istream input_stream(&str_buf);
+			std::getline(input_stream, client->m_message);
+			if (client->m_message == "EOF") {
+				std::lock_guard<std::mutex> lock(mutex_);
+				message_ = client->get_name() + "\r";
+				con_->notify_all();
+				break;
+			}
+			flag = true;
+			std::scoped_lock lock(mutex_);
+			message_ = client->m_message;
+			con_->notify_all();
+		}
+	}
+	void send(boost::asio::ip::tcp::socket socket, ClientOnServer* client) {
+		while (true) {
+			std::unique_lock lock(mutex_);
+			con_->wait(lock);
+			 client->add_message(&message_);
+			if (client->m_message == client->get_name() + '\r') {
+				boost::asio::write(socket, boost::asio::buffer("EOF\n"));
+				break;
+			}
+			if (client->m_message[client->m_message.size() - 1] == '\r'){
+				client->m_message.erase(client->m_message.size() - 1);
+				boost::asio::write(socket, boost::asio::buffer(client->m_message + " leave the chat\n"));
+			}
+			else {
+				if (!flag) boost::asio::write(socket, boost::asio::buffer(client->m_message + "\n"));
+				else flag = false;
+			}
+		}
+	}
 };
-int main() {
-    Server().start();
-    system("pause");
-    return EXIT_SUCCESS;
 
+int main()
+{
+	Server().start();
+	system("pause");
+	return EXIT_SUCCESS;
 }
