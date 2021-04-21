06ff5c05ef7afe889c125a37f56feab30e63c04a
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-04-21 03:06:10 +0300

    update client

--- client.cpp
+++ client.cpp
@@ -11,7 +11,6 @@
 class Client {
 private:
     std::atomic<bool> flag = false, end = false;
-    std::string output;
     std::string personal_name;
     std::string local_host = "127.0.0.1";
     int port = 3333;
@@ -20,19 +19,10 @@ private:
     boost::asio::ip::tcp::endpoint endpoint;
 public:
     explicit Client(): endpoint(boost::asio::ip::address::from_string(local_host), port), socket(io_service, endpoint.protocol()) {
-        try {
-            socket.connect(endpoint);
-            std::cout << "Please, enter your name to join the chat: ";
-            getline(std::cin, personal_name);
-            boost::asio::write(socket, boost::asio::buffer(personal_name + '\r'));
-        }
-        catch (boost::system::system_error& client_create)
-        {
-            std::cout << "Error occured! Error code = " << client_create.code() << ". Message: " << client_create.what() << std::endl;
-            std::cout << "AUF" << std::endl;
-            client_create.code().value();
-            system("pause");
-        }
+        socket.connect(endpoint);
+        std::cout << "Please, enter your name to join the chat: " << std::endl;
+        std::getline(std::cin, personal_name);
+        boost::asio::write(socket, boost::asio::buffer(personal_name + '\n'));
     }
     void start() {
         std::future<void> th1 = std::async(&Client::send, this);
@@ -43,43 +33,30 @@ public:
     }
     void send()
     {
-        do {
-            try
-            {
-                getline(std::cin, output);
-                boost::asio::write(socket,boost::asio::buffer(output+"\n"));
-
+        while (true) {
+            std::string output;
+            while (std::getline(std::cin, output)) {
+                boost::asio::write(socket, boost::asio::buffer("["+personal_name+"]" + " " + output + '\n'));
             }
-            catch (boost::system::system_error& send_client)
-            {
-                std::cout << "Error occured! Error code = " << send_client.code() << ". Message: " << send_client.what() << std::endl;
-                system("pause");
-                send_client.code().value();
-            }
-        } while (output != "Bye");
-        end = true;
+            output = "EOF";
+            boost::asio::write(socket, boost::asio::buffer(output + '\n'));
+        }
     }
     void get() {
         
         while (!end.load()) {
-                try
-                {
-                    boost::asio::streambuf str_buf;
-                    boost::asio::read_until(socket, str_buf, '\n');
-                    std::istream input_stream(&str_buf);
-                    std::string message;
-                    std::getline(input_stream, message);
-                    std::cout << message << std::endl;
-                   
+                boost::asio::streambuf str_buf;
+                boost::asio::read_until(socket, str_buf, '\n');
+                std::istream input_stream(&str_buf);
+                std::string message;
+                std::getline(input_stream, message);
+                if (message == "EOF") {
+                    break;
                 }
-                catch (boost::system::system_error& get_client)
-                {
-                    std::cout << "Error occured! Error code = " << get_client.code() << ". Message: " << get_client.what() << std::endl;
-                    system("pause");
-                    get_client.code().value();
-                }
+                std::cout << message << std::endl;
+            }
+
         }
-    }
    
         
 };
